88cf6a9ee70f89021bbdd9a1557d7b172361fb9d
Miguel Angel Vázquez <miguel.vazquez@noktos.com.mx>
2021-05-21 23:41:04 -0500

    Ajustes

--- app/Http/Controllers/Zoho/WebhookController.php
+++ app/Http/Controllers/Zoho/WebhookController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Zoho;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class WebhookController extends Controller
 {
@@ -16,9 +18,13 @@ class WebhookController extends Controller
     {
         \Log::debug(__FILE__);
         \Log::debug('REQUEST: '.PHP_EOL.json_encode($request->all()));
-        \Log::debug('SERVER: '.PHP_EOL.json_encode($_SERVER));
-        \Log::debug('GET: '.PHP_EOL.json_encode($_GET));
-        \Log::debug('POST: '.PHP_EOL.json_encode($_POST));
+        \Log::debug('SERVER: '.PHP_EOL.json_encode($_SERVER, JSON_PRETTY_PRINT));
+        \Log::debug('GET: '.PHP_EOL.json_encode($_GET, JSON_PRETTY_PRINT));
+        \Log::debug('POST: '.PHP_EOL.json_encode($_POST, JSON_PRETTY_PRINT));
+
+        $filename = Str::slug(\uniqid('', true)).'.json';
+
+        Storage::put('tmp/'.$filename, json_encode($request->all()));
 
         return \response()->json(array_merge(['time' => time()], $request->all()));
     }
