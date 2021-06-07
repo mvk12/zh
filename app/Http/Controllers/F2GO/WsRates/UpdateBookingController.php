f42b85e9185b6f32a9973a26366b10c53bc7ccba
Miguel Angel Vázquez <miguel.vazquez@noktos.com.mx>
2021-06-07 08:00:27 -0500

    Pruebas de ws_rates/update-booking

--- app/Http/Controllers/F2GO/WsRates/UpdateBookingController.php
+++ app/Http/Controllers/F2GO/WsRates/UpdateBookingController.php
@@ -3,32 +3,92 @@
 namespace App\Http\Controllers\F2GO\WsRates;
 
 use App\Http\Controllers\Controller;
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 
 class UpdateBookingController extends Controller
 {
     public function store(Request $request)
     {
-        $formData = $request->except('_token');
-        foreach (array_keys($formData) as $key) {
-            if ($formData[$key] == null) {
-                $formData[$key] = '';
-            }
-        }
+        $payload = [
+            '@attributes' => [
+                'xmlns:soap' => 'http://www.w3.org/2003/05/soap-envelope',
+                'xmlns:tem' => 'http://tempuri.org/',
+            ],
+            'soap:Header' => '',
+            'soap:Body' => [
+                'tem:UpdateBooking' => [
+                    'tem:sLnTy' => '',
+                    'tem:sCrTy' => '',
+                    'tem:sp_cod_hotel' => $request->input('sp_cod_hotel'),
+                    'tem:sp_clave_reserva' => $request->input('sp_clave_reserva'),
+                    'tem:sp_email' => $request->input('sp_email'),
+                    'tem:sp_nombre_titular' => [
+                        'tem:string' => [
+                            0 => '1',
+                            1 => $request->input('sp_nombre_titular'),
+                        ],
+                    ],
+                    'tem:sp_apellidos_titular' => [
+                        'tem:string' => [
+                            0 => '1',
+                            1 => $request->input('sp_apellidos_titular'),
+                        ],
+                    ],
+                    'tem:sp_adultostit' => [
+                        'tem:string' => [
+                            0 => '1',
+                            1 => $request->input('sp_adultostit'),
+                        ],
+                    ],
+                    'tem:sp_cod_thab' => $request->input('sp_cod_thab'),
+                    'tem:dp_fecha_entrada' => $request->input('dp_fecha_entrada'),
+                    'tem:dp_fecha_salida' => $request->input('dp_fecha_salida'),
+                    'tem:ksp_cod_emis' => '',
+                    'tem:sp_nombre_acomp' => $request->filled('sp_nombre_acomp') ? ['tem:string' => ['1', $request->input('sp_nombre_acomp')]] : '',
+                    'tem:sp_apellidos_acomp' => $request->filled('sp_apellidos_acomp') ? ['tem:string' => ['1', $request->input('sp_apellidos_acomp')]] : '',
+                    'tem:sp_adulto' => [
+                        'tem:string' => $request->input('sp_adulto'),
+                    ],
+                    'tem:ip_rooms' => [
+                        'tem:int' => [
+                            0 => '1',
+                            1 => $request->input('ip_rooms'),
+                        ],
+                    ],
+                    'tem:NotasReservacion' => $request->input('NotasReservacion'),
+                ],
+            ],
+        ];
 
-        \Log::debug(__FILE__.PHP_EOL.json_encode($formData, JSON_PRETTY_PRINT));
+        $domDocument = \App\Utilities\LaLit\Array2XML::createXML('soap:Envelope', $payload);
+        $domDocument->formatOutput = false;
+        $xml = $domDocument->saveXML();
+        $xml = str_replace('<?xml version="1.0" encoding="utf-8" standalone="no"?>', '', $xml);
+        $xml = preg_replace('/(\t|\n|\n\r|\r)/', '', $xml);
+        \Log::debug(__FILE__.PHP_EOL.json_encode($payload, JSON_PRETTY_PRINT));
+        \Log::debug(__FILE__.PHP_EOL.$xml);
 
-        $URL = 'http://extended.front2go.online/WHSEngine/ws_rates.asmx/UpdateBooking';
-        $curl = curl_init();
-        curl_setopt_array($curl, [
-            CURLOPT_URL => $URL,
-            CURLOPT_POST => 1,
-            CURLOPT_POSTFIELDS => http_build_query($formData),
-            CURLOPT_RETURNTRANSFER => true,
+        $client = new Client([
+            'http_errors' => false,
         ]);
-        $response = curl_exec($curl);
-        curl_close($curl);
+
+        $URL = 'http://extended.front2go.online/WHSEngine/ws_rates.asmx';
+
+        $responseClient = $client->request(
+            'POST',
+            $URL,
+            [
+                'headers' => [
+                    'Content-Type' => 'application/soap+xml;charset=UTF-8;action="http://tempuri.org/UpdateBooking"',
+                ],
+                'body' => $xml,
+            ]
+        );
+
+        $response = (string) $responseClient->getBody();
         \Log::debug(__FILE__.PHP_EOL.$response);
+        \Log::debug(__FILE__.PHP_EOL.json_encode(\App\Utilities\LaLit\XML2Array::createArray($response), JSON_PRETTY_PRINT));
 
         return \response($response, 200, ['Content_Type' => 'text/plain']);
     }
--- resources/views/f2go/ws_rates/updatebooking.blade.php
+++ resources/views/f2go/ws_rates/updatebooking.blade.php
@@ -9,162 +9,134 @@
 </head>
 <body>
 <div class="container">
+  <div class="row">
+    <div class="col"><h1>WS_Rates - UpdateBooking</h1></div>
+  </div>
+  <form action="{{route('f2go.ws_rates.update-booking.store')}}" method="POST">
+    @csrf
     <div class="row">
-        <div class="col">
-            <form action="{{route('f2go.ws_rates.update-booking.store')}}" method="POST">
-              @csrf
-              <div class="row">
-                <div class="col">
-                  <div class="form-group">
-                    <label for="">sLnTy</label><input type="text" class="form-control"name="sLnTy" />
-                  </div>
-                </div>
-                <div class="col">
-                  <div class="form-group">
-                    <label for="">sCrTy</label><input type="text" class="form-control"name="sCrTy" />
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col">
-                  <div class="form-group">
-                    <label for="">sp_cod_hotel</label><input type="text" class="form-control"name="sp_cod_hotel" />
-                  </div>
-                </div>
-                <div class="col">
-                  <div class="form-group">
-                    <label for="">sp_clave_reserva</label><input type="text" class="form-control"name="sp_clave_reserva" placeholder="12345"/>
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col">
-                  <div class="form-group">
-                    <label for="">sp_email</label><input type="text" class="form-control"name="sp_email" />
-                  </div>
-                </div>
-                <div class="col">
-                  <div class="form-group">
-                    <label for="">sp_nombre_titular</label><input type="text" class="form-control"name="sp_nombre_titular" />
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col">
-                  <div class="form-group">
-                    <label for="">sp_apellidos_titular</label><input type="text" class="form-control"name="sp_apellidos_titular" />
-                  </div>
-                </div>
-                <div class="col">
-                  <div class="form-group">
-                    <label for="">sp_adultostit</label><input type="text" class="form-control"name="sp_adultostit" />
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col">
-                  <div class="form-group">
-                    <label for="">sp_cod_thab</label><input type="text" class="form-control"name="sp_cod_thab" />
-                  </div>
-                </div>
-                <div class="col">
-                  <div class="form-group">
-                    <label for="">dp_fecha_entrada</label><input type="text" class="form-control"name="dp_fecha_entrada" />
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col">
-                  <div class="form-group">
-                    <label for="">dp_fecha_salida</label><input type="text" class="form-control"name="dp_fecha_salida" />
-                  </div>
-                </div>
-                <div class="col">
-                  <div class="form-group">
-                    <label for="">ksp_cod_emis</label><input type="text" class="form-control"name="ksp_cod_emis" />
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col">
-                  <div class="form-group">
-                    <label for="">sp_nombre_acomp</label><input type="text" class="form-control"name="sp_nombre_acomp" />
-                  </div>
-                </div>
-                <div class="col">
-                  <div class="form-group">
-                    <label for="">sp_apellidos_acomp</label><input type="text" class="form-control"name="sp_apellidos_acomp" />
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col">
-                  <div class="form-group">
-                    <label for="">sp_adulto</label><input type="text" class="form-control"name="sp_adulto" />
-                  </div>
-                </div>
-                <div class="col">
-                  <div class="form-group">
-                    <label for="">ip_rooms</label><input type="text" class="form-control"name="ip_rooms" value="1"/>
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col">
-                  <div class="form-group">
-                    <label for="">NotasReservacion</label>
-                    <textarea class="form-control"name="NotasReservacion" ></textarea>
-                  </div>
-                </div>
-              </div>
-
-              <button type="submit" class="btn btn-success">Enviar</button>
-            </form>
-            </div>
+      <div class="col">
+        <div class="form-group">
+          <label for="">sLnTy</label>
+          <input type="text" class="form-control"name="sLnTy" readonly/>
         </div>
+      </div>
+      <div class="col">
+        <div class="form-group">
+          <label for="">sCrTy</label>
+          <input type="text" class="form-control"name="sCrTy" readonly/>
+        </div>
+      </div>
     </div>
+    <div class="row">
+      <div class="col">
+        <div class="form-group">
+          <label for="">sp_cod_hotel</label>
+          <input type="text" class="form-control"name="sp_cod_hotel" value=""/>
+        </div>
+      </div>
+      <div class="col">
+        <div class="form-group">
+          <label for="">sp_clave_reserva</label>
+          <input type="text" class="form-control"name="sp_clave_reserva" placeholder="12345" value=""/>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="form-group">
+          <label for="">sp_email</label>
+          <input type="text" class="form-control"name="sp_email" value=""/>
+        </div>
+      </div>
+      <div class="col">
+        <div class="form-group">
+          <label for="">sp_nombre_titular</label>
+          <input type="text" class="form-control"name="sp_nombre_titular" value="" />
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="form-group">
+          <label for="">sp_apellidos_titular</label>
+          <input type="text" class="form-control"name="sp_apellidos_titular" value="" />
+        </div>
+      </div>
+      <div class="col">
+        <div class="form-group">
+          <label for="">sp_adultostit</label>
+          <input type="text" class="form-control"name="sp_adultostit" value="" />
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="form-group">
+          <label for="">sp_cod_thab</label>
+          <input type="text" class="form-control"name="sp_cod_thab" value="" />
+        </div>
+      </div>
+      <div class="col">
+        <div class="form-group">
+          <label for="">dp_fecha_entrada</label>
+          <input type="text" class="form-control"name="dp_fecha_entrada" value="" />
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="form-group">
+          <label for="">dp_fecha_salida</label>
+          <input type="text" class="form-control"name="dp_fecha_salida" value=""/>
+        </div>
+      </div>
+      <div class="col">
+        <div class="form-group">
+          <label for="">ksp_cod_emis</label>
+          <input type="text" class="form-control"name="ksp_cod_emis" value="" readonly />
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="form-group">
+          <label for="">sp_nombre_acomp</label>
+          <input type="text" class="form-control"name="sp_nombre_acomp" value="" />
+        </div>
+      </div>
+      <div class="col">
+        <div class="form-group">
+          <label for="">sp_apellidos_acomp</label>
+          <input type="text" class="form-control"name="sp_apellidos_acomp" value="" />
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="form-group">
+          <label for="">sp_adulto</label>
+          <input type="text" class="form-control"name="sp_adulto" value=""/>
+        </div>
+      </div>
+      <div class="col">
+        <div class="form-group">
+          <label for="">ip_rooms</label>
+          <input type="text" class="form-control"name="ip_rooms" value=""/>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="form-group">
+          <label for="">NotasReservacion</label>
+          <textarea class="form-control"name="NotasReservacion" ></textarea>
+        </div>
+      </div>
+    </div>
+
+    <button type="submit" class="btn btn-success">Enviar</button>
+  </form>
 </div>
 </body>
 </html>
-
-{{--
-// $str = 'sLnTy=string&sCrTy=string&sp_cod_hotel=string&sp_clave_reserva=string&sp_email=string&sp_nombre_titular=string&sp_nombre_titular=string&sp_apellidos_titular=string&sp_apellidos_titular=string&sp_adultostit=string&sp_adultostit=string&sp_cod_thab=string&dp_fecha_entrada=string&dp_fecha_salida=string&ksp_cod_emis=string&sp_nombre_acomp=string&sp_nombre_acomp=string&sp_apellidos_acomp=string&sp_apellidos_acomp=string&sp_adulto=string&sp_adulto=string&ip_rooms=string&ip_rooms=string&NotasReservacion=string';
-
-// $data = [];
-// parse_str($str, $data);
-
-// var_dump($data);
-
-$formData = [
-    'sLnTy' => '',
-    'sCrTy' => '',
-    'sp_cod_hotel' => 'DEMO',
-    'sp_clave_reserva' => '316612',
-    'sp_email' => '',
-    'sp_nombre_titular' => '',
-    'sp_apellidos_titular' => '',
-    'sp_adultostit' => '',
-    'sp_cod_thab' => '',
-    'dp_fecha_entrada' => '2021-06-15',
-    'dp_fecha_salida' => '2021-06-30',
-    'ksp_cod_emis' => '',
-    'sp_nombre_acomp' => '',
-    'sp_apellidos_acomp' => '',
-    'sp_adulto' => '',
-    'ip_rooms' => '',
-    'NotasReservacion' => 'Modificación de ejemplo',
-];
-
-$URL = '';
-$curl = curl_init();
-curl_setopt_array($curl, [
-    CURLOPT_URL => $URL,
-    CURLOPT_POST => 1,
-    CURLOPT_POSTFIELDS => http_build_query($formData),
-    CURLOPT_RETURNTRANSFER => TRUE,
-]);
-$string = curl_exec($curl);
-curl_close($curl);
-echo $string.PHP_EOL;
-
- --}}
